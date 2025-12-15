aef1e3a46c27a7476af3150f10a0bd6c165410de
Alexis-Ribat <alribat@hotmail.fr>
2025-12-15 01:50:48 +0100

    test

---
+++ mep.ps1
@@ -0,0 +1,9 @@
+﻿Write-Host "Generation du site..." -ForegroundColor Cyan
+hugo
+git add .
+$message = Read-Host "Message de la mise a jour (Entree pour 'Update')"
+if ($message -eq "") { $message = "Update rapide" }
+git commit -m "$message"
+Write-Host "Envoi vers GitHub..." -ForegroundColor Cyan
+git push origin main
+Write-Host "Termine ! Le site sera a jour dans 2 minutes." -ForegroundColor Green
